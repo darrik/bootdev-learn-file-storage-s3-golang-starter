4c25b9e90ad5bda8e0fd1ea5f96ae1f2cdf00d15
Rikard Glans <rikard@ecx.se>
2025-02-20 21:24:21 +0100

    ch1-l5

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
+
+const maxMemory = 10 << 20 // 10mb
+const ctmsg = "couldn't find Content-Type"
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 	videoIDString := r.PathValue("videoID")
@@ -28,10 +33,60 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	err = r.ParseMultipartForm(maxMemory)
+	file, fileheader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, 500, "error reading thumbnail", err)
+	}
+	defer file.Close()
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// fmt.Println("DEBUG: ", fileheader.Header)
+	// fmt.Println("DEBUG: ", fileheader.Filename)
+	// fmt.Println("DEBUG: ", fileheader.Size)
+
+	// mediaType := r.Header.Get("Content-Type")
+	// if mediaType == "" {
+	//	log.Printf(ctmsg)
+	//	respondWithError(w, 500, ctmsg, nil)
+	//	return
+	// }
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, 500, "error reading file", err)
+		return
+
+	}
+
+	videoInfo, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, 500, "error getting video information", err)
+		return
+	}
+
+	if videoInfo.UserID != userID {
+		respondWithJSON(w, http.StatusUnauthorized, struct{}{})
+	}
+
+	ct := fileheader.Header.Get("Content-Type")
+	th := thumbnail{
+		data: data,
+		mediaType: ct,
+	}
+
+	// fmt.Println("DEBUG: ", th.mediaType)
+
+	videoThumbnails[videoID] = th
+
+	url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", os.Getenv("PORT"), videoID)
+	videoInfo.ThumbnailURL = &url
+	err = cfg.db.UpdateVideo(videoInfo)
+	if err != nil {
+		respondWithError(w, 500, "error updating video metadata", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, videoInfo)
 }
